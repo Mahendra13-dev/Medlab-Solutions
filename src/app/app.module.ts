6c41fc7d9c86ae0f3d9b8d672c958d1e14ec04c8
Mahendra <mgaikwad1313@gmail.com>
2024-10-13 19:46:32 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
+import { TopDealsComponent } from './components/top-deals/top-deals.component';
+import { ShopByCategoryComponent } from './components/shop-by-category/shop-by-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MedicineHomeComponent,
+    TopDealsComponent,
+    ShopByCategoryComponent
   ],
   imports: [
     BrowserModule,
